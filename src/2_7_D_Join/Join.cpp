7b08e999b2ad5832ec68a8f87176cdf621d98583
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-17 00:10:40 +0500

    refactor: enhance Join to support multiple tokens and delimiter

--- src/2_7_D_Join/Join.cpp
+++ src/2_7_D_Join/Join.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <vector>
 
-std::string Join(const std::vector<std::string>& tokens, char
-                 // delimiter
-) {
+std::string Join(const std::vector<std::string>& tokens, char delimiter) {
   if (tokens.empty()) return "";
-  return tokens.back();
+  std::string sum;
+  for (auto& token : tokens) {
+    if (&token != &tokens[0]) sum += delimiter;
+    sum += token;
+  }
+  return sum;
 }
