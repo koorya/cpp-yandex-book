4087e49db27ad409e6cee2a8ffb00a1f5c88f771
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-17 00:12:33 +0500

    refactor: improve variable names in Join function

--- src/2_7_D_Join/Join.cpp
+++ src/2_7_D_Join/Join.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 
 std::string Join(const std::vector<std::string>& tokens, char delimiter) {
-  std::string sum;
+  std::string joinedTokens;
   for (auto& token : tokens) {
-    if (&token != &tokens[0]) sum += delimiter;
-    sum += token;
+    if (&token != &tokens[0]) joinedTokens += delimiter;
+    joinedTokens += token;
   }
-  return sum;
+  return joinedTokens;
 }
