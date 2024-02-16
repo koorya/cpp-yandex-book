276e9ff6af42a4c71e8364b383c2b2d04fb8d43d
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-16 23:58:54 +0500

    test: add case for single string in Join

--- src/2_7_D_Join/.test/Join.test.cpp
+++ src/2_7_D_Join/.test/Join.test.cpp
@@ -2,4 +2,8 @@
 
 #include <gtest/gtest.h>
 
-TEST(Join, test_can_run) { EXPECT_EQ(1, 1); };
+TEST(Join, test_can_run) { EXPECT_EQ(1, 1); }
+
+TEST(Join, single_string) {
+  EXPECT_EQ(Join({"hello world"}, ' '), "Hello world");
+}
