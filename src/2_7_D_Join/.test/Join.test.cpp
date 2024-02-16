cee509584cfa1cbd76fa981f283b174589f118bd
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-17 00:02:36 +0500

    test: add case for empty vector in Join

--- src/2_7_D_Join/.test/Join.test.cpp
+++ src/2_7_D_Join/.test/Join.test.cpp
@@ -7,3 +7,5 @@ TEST(Join, test_can_run) { EXPECT_EQ(1, 1); }
 TEST(Join, single_string) {
   EXPECT_EQ(Join({"hello world"}, ' '), "hello world");
 }
+
+TEST(Join, empty_vector) { EXPECT_EQ(Join({}, ' '), ""); }
