5304f0ffa46cff40743cede08becb4862f043321
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-17 00:11:14 +0500

    refactor: remove unnecessary empty check in Join

--- src/2_7_D_Join/Join.cpp
+++ src/2_7_D_Join/Join.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 
 std::string Join(const std::vector<std::string>& tokens, char delimiter) {
-  if (tokens.empty()) return "";
   std::string sum;
   for (auto& token : tokens) {
     if (&token != &tokens[0]) sum += delimiter;
