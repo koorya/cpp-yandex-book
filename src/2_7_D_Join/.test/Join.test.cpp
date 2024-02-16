133562f85a7d92b166e78024dd16ef51d0f8ab9e
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-16 23:56:23 +0500

    feat: implement Join and add unit test

---
+++ src/2_7_D_Join/.test/Join.test.cpp
@@ -0,0 +1,5 @@
+#include "../Join.h"
+
+#include <gtest/gtest.h>
+
+TEST(Join, test_can_run) { EXPECT_EQ(1, 1); };
---
+++ src/2_7_D_Join/Join.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+std::string Join(const std::vector<std::string>&
+                 // tokens
+                 ,
+                 char
+                 // delimiter
+) {
+  return "";
+}
