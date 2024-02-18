6d0c95da0590c5f8ce0aa2a27e47a0b4f0258481
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-18 17:04:58 +0500

    feat: implement transpose algorythm

--- src/2_7_E_Transpose/Transpose.cpp
+++ src/2_7_E_Transpose/Transpose.cpp
@@ -3,5 +3,17 @@
 
 std::vector<std::vector<int>> Transpose(
     const std::vector<std::vector<int>>& matrix) {
-  return matrix;
+  auto n = matrix.size();
+  auto m = matrix.front().size();
+
+  auto foo = std::vector<int>(n);
+  auto t_matrix = std::vector<std::vector<int>>(m, foo);
+
+  for (size_t i = 0; i != n; ++i) {
+    for (size_t j = 0; j != m; ++j) {
+      t_matrix[j][i] = matrix[i][j];
+    }
+  }
+
+  return t_matrix;
 }
