bff6c4330a7b0d2e9fa3eac3eb459119a5479732
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-18 17:19:26 +0500

    test(matrix-transposition): add tests

--- src/2_7_E_Transpose/.test/Transpose.test.cpp
+++ src/2_7_E_Transpose/.test/Transpose.test.cpp
@@ -17,3 +17,46 @@ TEST(Transpose, one_x_two) {
   auto transposed = std::vector<std::vector<int>>(2, std::vector<int>(1, 1));
   EXPECT_EQ(Transpose(input), transposed);
 }
+
+TEST(Transpose, matrix_4_x_3) {
+  using VectorType = std::vector<int>;
+  using MatrixType = std::vector<VectorType>;
+
+  // clang-format off
+  MatrixType input = MatrixType{
+    VectorType{ 1,  2,  3,  4},
+    VectorType{ 5,  6,  7,  8},
+    VectorType{ 9, 10, 11, 12},
+  };
+
+  MatrixType transposed = MatrixType{
+    VectorType{ 1,  5,  9},
+    VectorType{ 2,  6, 10},
+    VectorType{ 3,  7, 11},
+    VectorType{ 4,  8, 12},
+  };
+  // clang-format on
+
+  EXPECT_EQ(Transpose(input), transposed);
+}
+
+TEST(Transpose, matrix_3_x_5) {
+  using VectorType = std::vector<int>;
+  using MatrixType = std::vector<VectorType>;
+
+  // clang-format off
+  MatrixType input = MatrixType{
+    VectorType{ 1,  5,  9},
+    VectorType{ 2,  6, 10},
+    VectorType{ 3,  7, 11},
+    VectorType{ 4,  8, 12},
+  };
+  MatrixType transposed = MatrixType{
+    VectorType{ 1,  2,  3,  4},
+    VectorType{ 5,  6,  7,  8},
+    VectorType{ 9, 10, 11, 12},
+  };
+  // clang-format on
+
+  EXPECT_EQ(Transpose(input), transposed);
+}
