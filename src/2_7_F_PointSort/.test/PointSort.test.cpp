e75d1b3d055220b7ef73ebef87cda519e9b48b1e
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-18 18:26:17 +0500

    test: Add tests for sorting two points

--- src/2_7_F_PointSort/.test/PointSort.test.cpp
+++ src/2_7_F_PointSort/.test/PointSort.test.cpp
@@ -8,3 +8,33 @@ TEST(PointSort, test_can_run) {
   auto expectedResult = PointList{Point{1, 1}};
   EXPECT_EQ(SortPoints(PointList{Point{1, 1}}), expectedResult);
 }
+
+TEST(PointSort, sort_two_points_already_sorted) {
+  auto expectedResult = PointList{
+      // clang-format off
+    Point{1, 1},
+    Point{2, 2}
+      // clang-format on
+  };
+  EXPECT_EQ(SortPoints(PointList{Point{1, 1}, Point{2, 2}}), expectedResult);
+}
+
+TEST(PointSort, sort_two_points_inverse_sorted) {
+  auto expectedResult = PointList{
+      // clang-format off
+    Point{1, 1},
+    Point{2, 2}
+      // clang-format on
+  };
+  EXPECT_EQ(SortPoints(PointList{Point{2, 2}, Point{1, 1}}), expectedResult);
+}
+
+TEST(PointSort, sort_two_points_task) {
+  auto expectedResult = PointList{
+      // clang-format off
+    Point{1, 2},
+    Point{2, 3}
+      // clang-format on
+  };
+  EXPECT_EQ(SortPoints(PointList{Point{2, 3}, Point{1, 2}}), expectedResult);
+}
