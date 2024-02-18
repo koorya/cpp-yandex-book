fc5673f8a199ee685a729218af278dc502d77001
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-18 16:28:20 +0500

    test: Add test case for Transpose function

--- src/2_7_E_Transpose/.test/Transpose.test.cpp
+++ src/2_7_E_Transpose/.test/Transpose.test.cpp
@@ -2,4 +2,12 @@
 
 #include <gtest/gtest.h>
 
+#include <vector>
+
 TEST(Transpose, test_can_run) { EXPECT_EQ(1, 1); }
+
+TEST(Transpose, one_x_one) {
+  auto input = std::vector<std::vector<int>>(1, std::vector<int>(1, 1));
+
+  EXPECT_EQ(Transpose(input), input);
+}
