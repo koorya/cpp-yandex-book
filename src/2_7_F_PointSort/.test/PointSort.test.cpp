71bfcc2a8da3407283cadffcfc47e3d5a0a38153
Kirill Ivanov <Puaktopblm@gmail.com>
2024-02-18 17:43:42 +0500

    feat: Add PointSort unit test and CMakeLists.txt

---
+++ src/2_7_F_PointSort/.test/PointSort.test.cpp
@@ -0,0 +1,7 @@
+#include "../PointSort.h"
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+TEST(PointSort, test_can_run) { EXPECT_EQ(1, 1); }
---
+++ src/2_7_F_PointSort/PointSort.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+using Point = std::pair<int, int>;
+using PointList = std::vector<Point>;
+
+PointList SortPoints(const PointList& points);
